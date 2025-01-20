2d72355dc05a0f61eed1a651f941c493a17e75e2
tsj7ww <20461601+tsj7ww@users.noreply.github.com>
2025-01-20 14:31:01 -0500

    Update compile.sh

--- docs/compile.sh
+++ docs/compile.sh
@@ -11,7 +11,7 @@ projects=(
 if [ -f docs/intro.md ]; then
     cat docs/intro.md > README.md
     echo "" >> README.md
-    echo "<br>" >> README.md
+    # echo "<br>" >> README.md
     echo "" >> README.md
 fi
 
