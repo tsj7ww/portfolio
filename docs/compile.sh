d6ee95cd02a03bd9fbf2c90d26b27ae539145f2f
Trevor Jordan <20461601+tsj7ww@users.noreply.github.com>
2025-01-20 13:24:59 -0500

    auto build

---
+++ docs/compile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.." || exit 1
+
+projects=(
+    "baseball"
+    "loss_aversion"
+    "auto_ml"
+)
+
+if [ -f docs/intro.md ]; then
+    cat docs/intro.md > README.md
+    echo "" >> README.md
+fi
+
+process_readme() {
+    local project=$1
+    local dir="projects/$project"
+    
+    if [ -f "$dir/README.md" ]; then
+        printf "##" >> README.md
+        # cat "$dir/README.md" >> README.md
+        first_line=$(head -n 1 "$dir/README.md")
+        prefix=${first_line:0:2}
+        title=${first_line:2} 
+        remaining_content=$(tail -n +2 "$dir/README.md")
+        printf "${prefix}[${title}](./$dir)" >> README.md
+        echo "$remaining_content" >> README.md
+        echo "" >> README.md
+        echo "" >> README.md
+    else
+        echo "Warning: README.md not found in $dir" >&2
+    fi
+}
+
+for project in "${projects[@]}"; do
+    process_readme "$project"
+done
+
+echo "---" >> README.md
--- projects/loss_aversion/README.md
+++ projects/loss_aversion/README.md
@@ -1,1 +1,5 @@
+# Loss Aversion Analysis
 
+Data Science & Risk Management project using various baseball data sources to predict game outcomes and make informed sports bets. Predictions are generated using statistical models in a two-tiered approach - one at the game level, and another using a Discrete Event Simulator. The predictions are then compared against market odds to identify games with the highest potential ROI from placing bets. This is assessed across all games in a given time period, and bets are placed strategically in order to minimize downside risk while still realizing significant profits.
+- **Skills**: Statistical Modeling, Prediction, Risk Management, Problem Solving, Event Simulation
+- **Technology**: Python, XG Boost (Gradient Boosted Decision Tree) Model, Neural Network
